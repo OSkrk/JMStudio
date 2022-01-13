7cfac10de9189093fb95ffd3d91b9a7652c8622f
OSkrk <63248433+OSkrk@users.noreply.github.com>
2022-01-14 00:16:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+window.onload = (event) => {
+  set_size();
+};
+
 function check() {
   set_size();
     }
@@ -10,12 +14,13 @@ function set_size(){
    if (wid > hei){ //This is wide screen
   $('.background-intro-img').width(wid);
   $('.background-intro-img').height(wid*0.6);
+  console.log(" wide screen");
   }
   else{ // this vertical screen : this case shall crop the image from the right
+    console.log("vertical screen");
     $('.background-intro-img').width(wid*2);
     $('.background-intro-img').height(wid*0.6*2);
-    $('.background-intro-img').attr("margin-left", "-100%");
-
+    $('.background-intro-img').attr("margin-right","-100%");
   }
 }
 // Check if the window changed size
