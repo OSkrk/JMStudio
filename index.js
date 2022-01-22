e748673f49bc410d5a857b3a69a082a9fb647a82
OSkrk <63248433+OSkrk@users.noreply.github.com>
2022-01-22 22:30:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,26 +11,31 @@ check(); // first-time check
 function set_size(){
   var wid = $( window ).innerWidth();
   var hei = $( window ).innerHeight();
-   if (wid > hei){ //This is wide screen
+  var ratio = wid/hei;
+   if (ratio > 1.5){ //This is wide screen
   $('.background-intro-img').width(wid);
   $('.background-intro-img').height(wid*0.6);
+  $('.bio-background-img').width(wid);
+  $('.bio-background-img').height(wid*0.55);
   $('.tittle-text').removeClass("tt-vertical").addClass("tt-wide");
   $('.top-right-slogan').removeClass("trs-vertical").addClass("trs-wide");
   $('.services-list').removeClass("sl-vertical").addClass("sl-wide");
   $('.social-media').removeClass("sm-vertical").addClass("sm-wide");
   $('.navbar_company_section').removeClass("ncs-vertical").addClass("ncs-wide");
-  console.log(" wide screen");
+
   }
   else{ // this vertical screen : this case shall crop the image from the right
-    console.log("vertical screen");
     $('.background-intro-img').width(wid*2);
     $('.background-intro-img').height(wid*0.6*2);
+    $('.bio-background-img').width(wid);
+    $('.bio-background-img').height(wid*0.55);
     $('.tittle-text').removeClass("tt-wide").addClass("tt-vertical");
     $('.top-right-slogan').removeClass("trs-wide").addClass("trs-vertical");
     $('.services-list').removeClass("sl-wide").addClass("sl-vertical");
     $('.social-media').removeClass("sm-wide").addClass("sm-vertical");
     $('.navbar_company_section').removeClass("ncs-wide").addClass("ncs-vertical");
   }
+  console.log( "widith: " + wid + " height: " + hei + "ratio: " + wid/hei);
 }
 // Check if the window changed size
   $( window ).resize(function() {
